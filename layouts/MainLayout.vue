12652892c6b8785e8fd24c9e1dffbd43826ba626
Dung Tran <t2adung@gmail.com>
2024-09-25 10:29:08 +0700

    Post Main Components

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,7 +1,13 @@
 <template>
     <TopNav/>
+    <div :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''" class="flex justify-between mx-auto w-full lg:px-2.5 px-0">
+        <div>
+            <SideNavMain/>
+        </div>
+        <slot />
+    </div>
 </template>
 
 <script setup>
-
+    const route = useRoute()
 </script>
